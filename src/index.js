d79525f53e69375205a732911dcff832cfbb4631
VITALI STARASTSENKA <existing9@mail.ru>
2023-01-23 19:45:55 +0300

    feat: add solve towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,27 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix === undefined) return [];
+  if(matrix.length === 0) return [];
+  let i =0;
+  let res = [];
+  for(ar of matrix){
+   if(i%2) res[i] = ar.sort((a,b)=>b-a);
+   else res[i] = ar;
+   i++;
+  }
+  res = res.join().split(',').map((n)=>Number(n))
+  return res;
 }
+const matrix = [];
+ let i =0;
+ let res = [];
+ if(matrix.length === 0) res = [];
+
+ for(ar of matrix){
+  if(i%2) res[i] = ar.sort((a,b)=>b-a);
+  else res[i] = ar;
+  i++;
+ }
+ res = res.join().split(',').map((n)=>Number(n))
+console.log(res)
